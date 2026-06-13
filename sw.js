fa6e71fb3770cc1cf57ed9a31f669860feaecb4b
KishanIndrakumar <kishanindra@gmail.com>
2026-06-13 20:18:12 +0100

    Update cache version to pepguide-v2026.5

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *   update cache in background for next visit
  */
 
-const CACHE = 'pepguide-v2026.4';
+const CACHE = 'pepguide-v2026.5';
 
 self.addEventListener('install', event => {
   event.waitUntil(
