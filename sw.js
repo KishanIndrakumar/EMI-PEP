e34b0d9660c429e2e146170c2ad12e343bdfbbd1
KishanIndrakumar <kishanindra@gmail.com>
2026-07-08 02:09:40 +0530

    Fix cache version format in service worker

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *   update cache in background for next visit
  */
 
-const CACHE = 'pepguide-v2026.9';
+const CACHE = 'pepguide-v2026.09';
 
 self.addEventListener('install', event => {
   event.waitUntil(
